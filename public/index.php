de73899687a44e7c70b84e48f43ea19058dece3f
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-06-16 16:02:55 +0200

    displaying information from the calendar class

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 include_once '../sys/core/init.int.php';
 
-$cal = new Calendar($dbo, "20   16-01-01 12:00:00");
+$cal = new Calendar($dbo, "20   18-06-01 12:00:00");
 
 if (is_object($cal)){
     echo "<pre>", var_dump($cal), "</pre>";
