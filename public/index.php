13aa2a13ec41454d11518e5513b4fc1f3685356c
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-06-19 20:34:40 +0200

    footer to index added

--- public/index.php
+++ public/index.php
@@ -6,4 +6,10 @@ include_once '../sys/core/init.int.php';
 
 $cal = new Calendar($dbo, "20   18-06-01 12:00:00");
 
+$page_title = "Kalendarz wydarzeń";
+$css_files = array('style.css');
+$css_files[] = 'materialize.css';
+
+include_once 'assets/common/header.inc.php';
 echo $cal->buildCalendar();
+include_once 'assets/common/footer.inc.php';
