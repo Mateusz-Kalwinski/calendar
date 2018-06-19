0a5649da0c8158f8c1fe4b2c50e062549a96591c
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-06-19 20:36:19 +0200

    view of a single event

---
+++ public/view.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+if ( isset($_GET['event_id']) )
+{
+    $id = preg_replace('/[^0-9]/', '', $_GET['event_id']);
+
+    if ( empty($id) )
+    {
+        header("Location: ./");
+        exit;
+    }
+}
+else
+{
+    header("Location: ./");
+    exit;
+}
+
+include_once '../sys/core/init.int.php';
+
+$page_title = "Zobacz wydarzenie";
+$css_files = array("style.css");
+$css_files[] = 'materialize.css';
+
+include_once 'assets/common/header.inc.php';
+
+$cal = new Calendar($dbo);
+
+?>
+
+<div class="container">
+    <?=$cal->displayEvent($id)?>
+    <a href="./">Kalendarz</a>
+</div>
+
+<?php
+include_once 'assets/common/footer.inc.php';
+?>
