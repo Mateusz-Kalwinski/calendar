afe4c474c0cddbde5fd4a0e9058e2b63d73652e4
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-06-24 17:10:52 +0200

    hash password test

---
+++ public/test.php
@@ -0,0 +1,26 @@
+<?php
+//
+//declare(strict_types=1);
+//
+//include_once '../sys/core/init.int.php';
+//
+//$obj = new Admin($dbo);
+//
+//$hash1 = $obj->testSaltedHash("test");
+//
+//echo "skrót \$hash1 bez ciągu zaburzającego:<br />",$hash1, "<br /><br />";
+//
+//sleep(1);
+//
+//$hash2 = $obj->testSaltedHash('test');
+//
+//echo "skrót \$hash2 bez ciągu zaburzającego:<br />",$hash2, "<br /><br />";
+//
+//sleep(1);
+//
+//$hash3 = $obj->testSaltedHash('test', $hash2);
+//
+//echo "skrót \$hash3 bez ciągu zaburzającego:<br />",$hash3, "<br /><br />"
+//
+//$pass = $obj->testSaltedHash('admin');
+//echo "skrót \$pass bez ciągu zaburzającego:<br />",$pass, "<br /><br />";
