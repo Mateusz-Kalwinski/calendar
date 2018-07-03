b0652b2a01114396ba3929fce1e42eebc5718bcb
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-07-03 21:20:08 +0200

    a form is displayed here

---
+++ public/addUser.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+include_once '../sys/core/init.int.php';
+
+if (!isset($_SESSION['user'])){
+    header('Location: login.php');
+    exit();
+}
+
+$page_title = 'Dodaj/Edytuj wydarzenie';
+$css_files =  array('style.css');
+$css_files[] = 'materialize.css';
+
+include_once 'assets/common/header.inc.php';
+
+$cal = new Calendar($dbo);
+
+?>
+
+    <div class="container">
+        <?php
+        echo $cal->displayFormAddUser()
+        ?>
+    </div>
+<?php
+include_once 'assets/common/footer.inc.php'
+?>
