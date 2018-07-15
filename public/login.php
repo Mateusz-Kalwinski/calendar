737652c8d41d5896553637be08e929b8db664646
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-07-15 11:43:54 +0200

    if you are logged in, you can not enter login

--- public/login.php
+++ public/login.php
@@ -8,31 +8,33 @@ $page_title = 'Zaloguj się';
 $css_files = array('style.css');
 $css_files[] = 'materialize.css';
 include_once 'assets/common/header.inc.php';
-
+if (isset($_SESSION['user'])){
+    header("Location: ./");
+}
 ?>
 
 <div class="container">
-    <form class="forms" action="assets/inc/process.inc.php" method="post" autocomplete="off">
-            <div class="input-field">
-                <i class="material-icons prefix">account_circle</i>
-                <input id="uname" type="text" class="validate" name="uname" value="" autocomplete="off">
-                <label for="uname">Nazwa użytkownika</label>
-            </div>
-            <div class="input-field">
-                <i class="material-icons prefix">mode_edit</i>
-                <input id="pword" type="password" class="validate" name="pword" value="" autocomplete="off">
-                <label for="pword">Hasło</label>
-            </div>
-            <input type="hidden" name="token" value="<?=$_SESSION['token']?>">
-            <input type="hidden" name="action" value="user_login">
-            <div class="center-login">
-                <div class="col s8">
-                    <button class="waves-effect waves-light btn padding-1" type="submit" name="login_submit"><i class="material-icons left">send</i>Zaloguj się</button>
-                    <button class="waves-effect waves-light btn red accent-2 padding-1"><i class="material-icons left">cancel</i><a class="white-text" href="./">Anuluj</a></button>
-                </div>
+    <form class="form-login z-depth-5" action="assets/inc/process.inc.php" method="post" autocomplete="off">
+        <div class="input-field">
+            <i class="material-icons prefix indigo-text text-lighten-2">account_circle</i>
+            <input id="uname" type="text" class="validate" name="uname" value="" autocomplete="off">
+            <label for="uname">Nazwa użytkownika</label>
+        </div>
+        <div class="input-field">
+            <i class="material-icons prefix indigo-text text-lighten-2">fingerprint</i>
+            <input id="pword" type="password" class="validate" name="pword" value="" autocomplete="off">
+            <label for="pword">Hasło</label>
+        </div>
+        <input type="hidden" name="token" value="<?=$_SESSION['token']?>">
+        <input type="hidden" name="action" value="user_login">
+        <div class="center-login">
+            <div class="col s8">
+                <button class="waves-effect waves-light btn padding-1" type="submit" name="login_submit"><i class="material-icons left">send</i>Zaloguj się</button>
+                <button class="waves-effect waves-light btn red accent-2 padding-1"><i class="material-icons left">cancel</i><a class="white-text" href="./">Anuluj</a></button>
             </div>
         </div>
     </form>
+</div>
 <?php
 include_once 'assets/common/footer.inc.php';
 ?>
